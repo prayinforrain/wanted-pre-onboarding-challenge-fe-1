417f1e7a10ba29953ec5fd2614e5a588333a3120
PrayinForRain <dldnwo99@naver.com>
2023-01-06 19:18:26 +0900

    🐛 fix: 로그인 상태에서 auth 페이지에 접근할 수 있음

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import LoginBox from "../components/auth/loginBox";
 import "../styles/auth.scss";
 
 export default function AuthPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("auth")) {
+      navigate("/");
+    }
+  }, []);
   return (
     <div className="page">
       <LoginBox />
